ad7daabf6eb406af27e2a6ee1e1c08db27081ec7
Fab <fab@mail.fr>
2018-08-10 16:15:56 +0200

    1st commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/test.html.twig',
+            [
+              'nom' => 'Fabian'
+            ]
+        );
+    }
+
+    /**
+     * @Route("/page1", name="page1")
+     */
+    public function page1Action(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/test.html.twig');
+    }
+}
