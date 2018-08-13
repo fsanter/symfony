563b5a0696d42241aaa99e95aa7b09b4e5b87d3a
Fab <fab@mail.fr>
2018-08-13 09:46:42 +0200

    nouveau controller app

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        return new Response("app test");
+    }
+}
