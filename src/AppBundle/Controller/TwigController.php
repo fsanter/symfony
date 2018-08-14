7d3c524f398286be83ff51ffcc8caca160a7b272
Fab <fab@mail.fr>
2018-08-14 14:46:02 +0200

    affichage de variable en twig + filtre

--- src/AppBundle/Controller/TwigController.php
+++ src/AppBundle/Controller/TwigController.php
@@ -13,7 +13,26 @@ class TwigController extends Controller
     * TWIG
     * Génération de template avec pseudo-langage de programmation
     */
+
+    /**
+     * @Route("/twig", name="twig")
+     */
     public function twigAction() {
-        return $this->render('twig/twig.html.twig');
+        $bonjour = "Bonjour tout le monde";
+        $tableau = ['élément 1', 'chaine2', 56, true, ['salut']];
+        $tableauAssociatif = [
+            'clenumero1' => "Valeur numéro 1",
+            'prenom' => 'Fab'
+        ];
+        $datetime = new \DateTime();
+
+        return $this->render('twig/twig.html.twig',
+            [
+                'bonjour' => $bonjour,
+                'tableau' => $tableau,
+                'tableauAssociatif' => $tableauAssociatif,
+                'datetime' => $datetime
+            ]
+        );
     }
 }
