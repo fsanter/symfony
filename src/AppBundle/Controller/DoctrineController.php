688a57cdce2b62abb886c04452416b489a15d459
Fab <fab@mail.fr>
2018-08-17 11:59:16 +0200

    crud article

--- src/AppBundle/Controller/DoctrineController.php
+++ src/AppBundle/Controller/DoctrineController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DoctrineController extends Controller
@@ -24,12 +25,83 @@ class DoctrineController extends Controller
      */
     public function createAction(Request $request)
     {
+        // créer un article : instanciation
         $article = new Article();
+        $article->setTitle("Titre article 1");
+        $article->setContent("Voici le contenu de l'article");
 
+        $now = new \DateTime();
+        $article->setCreatedAt($now);
+
+        $article->setEnabled(false);
+
+        // récupérer le manager de doctrine
+        // qui va permettre les échanges avec la BDD
+        $em = $this->getDoctrine()->getManager();
+
+        // indique à doctrine qu'il faut gérer cette objet
+        // en l'enregistrant lors du prochain flush
+        $em->persist($article);
+
+        // executer les requêtes sql des objets
+        // sur lesquels on a fait un persist
+        $em->flush();
+
+        // à partir d'ici, $article->getId() renvoie l'id généré
 
         return $this->render('doctrine/create.html.twig',
             [
                 'article' => $article
+            ]
+        );
+    }
+
+    /**
+     * Read entity
+     * @Route("/read", name="read")
+     */
+    public function readAction(Request $request)
+    {
+        // récupérer le manager doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        // récupérer en base un objet grâce à son identifiant
+        // getRepository correspond au type d'objet à récupérer
+        // le find est la méthode permettant de faire un
+        // SELECT * FROM Article WHERE id = {{id}}
+        $article = $em->getRepository('AppBundle:Article')
+                      ->find(1);
+
+        if (!$article instanceof Article) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('doctrine/create.html.twig',
+            [
+                'article' => $article
+            ]
+        );
+    }
+
+    /**
+     * Read entity
+     * @Route("/read-all", name="read_all")
+     */
+    public function readAllAction(Request $request)
+    {
+        // récupérer le manager doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        // récupérer en base un objet grâce à son identifiant
+        // getRepository correspond au type d'objet à récupérer
+        // le findAll est la méthode permettant de faire un
+        // SELECT * FROM Article
+        $articles = $em->getRepository('AppBundle:Article')
+                      ->findAll();
+
+        return $this->render('doctrine/read_all.html.twig',
+            [
+                'articles' => $articles
             ]
         );
     }
